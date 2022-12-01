2456e98555b19178ec785fa5482a0be88dad6aa8
Unakrad <116517857+Unakrad@users.noreply.github.com>
2022-12-01 14:29:48 +0300

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
 DOWNLOAD_TEXT_URL = "https://t.me/How_to_Download_From_Search_Bot/2"
 
    # Custom Caption Under Button #
-CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/c/GreyMattersBot"
+CAPTION_BUTTON = "Main Channel"
+CAPTION_BUTTON_URL = "https://t.me/Netflix_seriesbot1"
 
    # Auto Delete For Bot Sending Files #
